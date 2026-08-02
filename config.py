965cf8ac261f20229467ebe0a349f22f997ea1d4
Ehifred <107931652+ehifred@users.noreply.github.com>
2026-08-03 00:53:11 +0100

    Read config from environment variables

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = 'your-secret-key-change-this-later'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/complaint_db'
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-change-this-later')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'mysql+pymysql://root:@localhost/complaint_db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
