1468377746c73909fa2becb91cd3052ae0cbda3e
Ehifred <107931652+ehifred@users.noreply.github.com>
2026-08-03 01:19:03 +0100

    Create database tables on startup

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from app import create_app, db
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
-    
+    app.run(debug=True) 
